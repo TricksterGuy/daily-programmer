39ca3c551377120c3095ba6ee625bd6cd1ce2489
Brandon <tricksterguy@live.com>
2015-03-04 21:46:26 -0800

    I AM BENDER. PLEASE INSERT GIRDER.

---
+++ bender-2015-02-16.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        printf("Usage: bender binaryString\n");
+        printf("Now bite my shiny metal ass\n")
+        abort();
+    }
+
+    const char* input = argv[1];
+
+    while (*input)
+    {
+        char sum = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            sum <<= 1;
+            sum += input[i] - '0';
+        }
+        printf("%c", sum);
+        input += 8;
+    }
+}
